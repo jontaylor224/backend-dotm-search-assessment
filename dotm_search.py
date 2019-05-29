4b9f8d06f5ffeadd0084b5370ef6e848188740ad
Jon Taylor <jontaylor224@gmail.com>
2019-05-29 15:30:41 -0500

    added check for zip file, test files for fake dotm

--- dotm_search.py
+++ dotm_search.py
@@ -12,10 +12,14 @@ import os
 import zipfile
 
 
-def get_docm_text(file_path):
+def get_dotm_text(file_path):
     """ take path of docm file as argument, return the text"""
-    with zipfile.ZipFile(file_path) as document:
-        xml_content = document.read('word/document.xml')
+    if not zipfile.is_zipfile(file_path):
+        print('ERROR: ' + file_path + ' is not a compressed file.')
+        xml_content = None
+    else:
+        with zipfile.ZipFile(file_path) as document:
+            xml_content = document.read('word/document.xml')
     return xml_content
 
 
@@ -39,11 +43,11 @@ def main():
 
     file_list = (os.path.join(args.dir, file) for file in os.listdir(
         args.dir) if file.endswith('.dotm'))
-
+    # print(list(file_list))
     for file in file_list:
         files_searched += 1
-        file_text = get_docm_text(file)
-        if args.search_term in file_text:
+        file_text = get_dotm_text(file)
+        if file_text is not None and args.search_term in file_text:
             print 'Match found in file ' + file
             print '...' + file_text[file_text.index(args.search_term) -
                                     40:file_text.index(args.search_term)+len(args.search_term)+40] + '...'
